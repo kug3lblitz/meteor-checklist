a257756ee970429acd655413f7ed0a5388f6df47
Patrick Werner <pwerner2@gmail.com>
2015-04-22 18:49:32 -0400

    realized how incredibly useful comments can be

--- meteor-checklist.js
+++ meteor-checklist.js
@@ -1,11 +1,17 @@
+Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
-  // This code only runs on the client
-  Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
-  });
+    Template.body.helpers ({
+        tasks: function () {
+            return Tasks.find({});
+        }
+    });
+
+    Template.body.events({
+        "submit new.task": function (event) {
+            
+            //function called on form submit
+            
+        }
+    })
 }
